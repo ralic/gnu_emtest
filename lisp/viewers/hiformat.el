82f8766d3080e8dbe0b3096e9c24fd422840ff84
Tom Breton <tehom@panix.com>
2010-03-20 18:49:53 -0400

    Made another compiler warning go away

--- lisp/viewers/hiformat.el
+++ lisp/viewers/hiformat.el
@@ -28,10 +28,8 @@
 
 
 ;;;_ , Requires
-
-(when (not (fboundp 'rtest:deftest))
-    (defmacro rtest:deftest (&rest dummy))
-    (defmacro rtest:if-avail (&rest dummy)))
+(eval-when-compile
+   (require 'cl))
 
 ;;;_. Body
 ;;;_ , Types
@@ -116,131 +114,6 @@ ELS=1 must be a function taking 3 args (same signature as FUNC).
 	       ;;Do we need a separate statement to get the results?
 	       )))))
 
-;;;_   , Test data
-;;Define examples, some share the same functors.
-;;;_   , Tests
-(rtest:deftest hiformat:map
-
-   ;;Data includes "index", "first", "last" (but may include other
-   ;;elements) Can't neatly test that without creating "subset" for
-   ;;emtm.
-   '
-   (  "Situation: WRITEME.
-Response: WRITEME."
-      (progn)
-      )
-   (  "Situation: Func and separator are trivial, just write 1 or 0.
-PARAM: Separator is a formatted list.
-Response: As expected."
-      (progn
-	 (assert
-	    (equal
-	       (hiformat:map
-		  #'(lambda (&rest dummy)
-		       '("1"))
-		  '(a b c)
-		  :separator
-		  '("\n"))
-	       '("1""\n""1""\n""1"))
-	    t)
-	 t))
-   
-   (  "Situation: Func and separator are trivial, just write 1 or 0.
-PARAM: Separator is a function.
-Response: As expected."
-      (progn
-	 (assert
-	    (equal
-	       (hiformat:map
-		  #'(lambda (&rest dummy)
-		       '("1"))
-		  '(a b c)
-		  :separator
-		  #'(lambda (&rest dummy)
-		       '("\n")))
-	       '("1""\n""1""\n""1"))
-	    t)
-	 t))
-
-   (  "Situation: Func and separator-f see data.
-Response: As expected."
-      (progn
-	 (assert
-	    (equal
-	       (hiformat:map
-		  #'(lambda (dummy-1 dummy-2 immediate-data-x)
-		       (cond
-			  ((assq 'first immediate-data-x)
-			     '("FIRST"))
-			  ((assq 'last immediate-data-x)
-			     '("LAST"))
-			  (t '("OTHER"))))
-		   '(a b c)
-		  :separator
-		  #'(lambda (&rest dummy)
-		       '("\n")))
-	       '("FIRST""\n""OTHER""\n""LAST"))
-	    t)
-	 t))
-
-   ;;Test ()
-   ;;ELS=1 is given and length = 1
-
-   (  "PARAM: ELS=0 is given
-PARAM: LIST is empty
-Response: The ELS=0 form is printed."
-      (progn
-	 (assert
-	    (equal
-	       (hiformat:map
-		  #'(lambda (&rest dummy)
-		       '("1"))
-		  '()
-		  :separator
-		  #'(lambda (&rest dummy)
-		       '("\n"))
-		  :els=0 '("12" "144")
-		  )
-	       '("12" "144"))
-	    t)
-	 t))
-   
-   (  "PARAM: ELS=1 is given
-PARAM: LIST has 1 element
-Response: The ELS=1 function is called instead of the usual function."
-      (progn
-	 (assert
-	    (equal
-	       (hiformat:map
-		  #'(lambda (&rest dummy)
-		       '("1"))
-		  '(0)
-		  :separator
-		  #'(lambda (&rest dummy)
-		       '("\n"))
-		  :els=1 
-		  #'(lambda (dummy dummy-2)
-		       '("12" "144"))
-		  )
-	       '("12" "144"))
-	    t)
-	 t))
-   
-
-   ;;Sees data-loal as param
-   )
-
-;;;_  . loformat functions (Living in hiformat for the moment)
-;;;_   , loformat:print
-;;You can tell it's loformat because it inserts, rather than make a
-;;format string.
-(defun loformat:print (x)
-   ""
-   (typecase x
-      (cons
-	 (mapcar #'loformat:print x))
-      (string
-	 (insert x))))
 
 ;;;_. Footers
 ;;;_ , Provides
