8208953a92bdeb1dea807f2489e095dc86f9644c
Tom Breton (Tehom) <tehom@panix.com>
2010-07-05 15:07:17 -0400

    New tests for finding .el and .elc versions

---
+++ lisp/emtest/testhelp/mocks/libversion/examples/compiled.el
@@ -0,0 +1,7 @@
+;;Test file.  This file is to exist as ".el" and ".elc" forms, but
+;;only to be available in ".el" form in the stable copy.
+(defconst libversion:th:examples/compiled/load-file-name 
+   load-file-name 
+   "The load-file name" )
+
+(provide 'emtest/testhelp/mocks/libversion/examples/compiled)
