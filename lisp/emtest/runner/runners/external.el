12b2f918ac48736b04d827cf50519648a5a358e2
Tom Breton (Tehom) <tehom@panix.com>
2010-12-07 22:24:02 -0500

    expect: Towards making sequential interactions

--- lisp/emtest/runner/runners/external.el
+++ lisp/emtest/runner/runners/external.el
@@ -32,6 +32,95 @@
 
 
 ;;;_. Body
+;;;_ , Structures
+;;Other data may be added here, such as for variables that cases
+;;carry around.
+(defstruct (emtr:external-data
+	      (:conc-name emtr:external-data->)
+	      (:constructor emtr:make-external-data))
+   "Data telling us how to run the interaction sequence"
+   tq
+   result-f
+   timer
+   pending
+   prompt
+   testral-obj)
+;;;_  . emtr:interact-predata
+(defstruct (emtr:interact-predata
+	      (:conc-name emtr:interact-predata->)
+	      (:constructor emtr:make-interact-predata))
+   "Data telling us how to run one interaction"
+   question
+   form
+   timeout)
+
+
+;;;_ , Support
+;;;_  . emtr:external-cb
+(defun emtr:external-cb (data answer)
+   "Callback for tq-enqueue"
+   (when (first data)
+      (emtt:testral:continued-with (second data)
+	 (emth:abortscope
+	    aborted-p
+	    (emth:trap-errors 
+	       (eval (first data)))
+	    t))))
+
+;;;_  . emtr:external-start-next
+(defun emtr:external-start-next (data)
+   ""
+   
+   (if
+      (emtr:external-data->pending data)
+      (let
+	 ((next
+	     (pop (emtr:external-data->pending data)))
+	    timer)
+	 (emtt:testral:continued-with 
+	    (emtr:external-data->testral-obj data)
+	    ;;Make a note of what the next thing is expected to send.
+	    ;;$$PUNT For now, as a doc note.
+	    (emt:doc (emtr:interact-predata->question next))
+	    (setf
+	       (emtr:external-data->timer data)
+	       ;;$$PUNT 
+	       '(run-at-time 
+		   (emtr:interact-predata->timeout next)
+		   nil
+		   #'(lambda (data)
+			;;Note a fail
+			;;Pop tq
+			;;Start another.
+			(emtr:external-start-next data))
+		   data))
+	    
+	    '
+	    (tq-enqueue my-tq 
+	       (emtr:interact-predata->question next)
+	       (emtr:external-data->prompt data)
+	       (list 
+		  (emtr:interact-predata->form next) 
+		  (emtr:external-data->testral-obj data))
+	       #'emtr:external-cb
+	       nil)))
+      
+      ;;Untested
+      '(progn
+	  ;;Otherwise report results
+	  (funcall report-f
+	     (emt:testral:make-suite
+		:contents
+		(emtt:testral:continued-with 
+		   (emtr:external-data->testral-obj data)
+		   (emtt:testral:get-notes))
+		:badnesses '()
+		:info '()))
+	  ;;, close tq, and we're done.
+	  (tq-close (emtr:external-data->tq data)))))
+
+
+
 ;;;_ , Entry points (for clause explorer)
 ;;;_  . emtr:external
 (defun emtr:external (props form report-f)
@@ -68,12 +157,37 @@
    (tq-create my-proc))
 
 '
+(setq my-data
+   (emtr:make-external-data
+      :tq my-tq
+      :result-f 'no-result-f-yet
+      :timer 'no-timer-yet
+      :pending 
+      (list
+	 (emtr:make-interact-predata
+	    :question 
+	    "echo hello\n"
+	    :form
+	    '(progn
+	       (emt:doc "Another note")
+	       (assert (equal answer 5) t))
+	    :timeout 
+	    10))
+      
+      :prompt "% "
+      :testral-obj my-con))
+
+'
+(emtr:external-start-next
+   my-data)
+
+'
 (tq-enqueue my-tq 
     "PS1='% '\n"
     "% "
-    my-con
+    (list nil my-con)
     #'(lambda (data answer)
-	 (emtt:testral:continued-with data
+	 (emtt:testral:continued-with (first data)
 	    (emt:doc "This note should go OK")))
    
     t)
@@ -84,13 +198,27 @@ my-con
 (tq-enqueue my-tq 
    "echo hello\n"
    "% "
-   57
-   #'(lambda (data answer)
-	(setq my-56-a (list data answer)))
-   
-          t)
+   (list 
+      '(progn
+	  (emt:doc "Another note")
+	  (assert (equal answer 5) t))
+      my-con )
+   #'emtr:external-cb
+   t)
 '
-my-56-a
+my-con
+
+' ;;No test.
+(tq-enqueue my-tq 
+   "echo hello\n"
+   "% "
+   (list my-con nil)
+   #'emtr:external-cb
+   t)
+
+'
+(emtt:testral:continued-with my-con
+    (emtt:testral:get-notes))
 
 ;;Cleanup: 
 '
